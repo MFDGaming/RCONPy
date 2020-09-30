c94dea4df93b469c050bec9fe34cc49f6fd02fb9
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-30 19:43:01 +0300

    Create ServerInterface.py

---
+++ rconpy/server/ServerInterface.py
@@ -0,0 +1,12 @@
+class ServerInterface:
+    def onOpenConnection(self, address):
+        pass
+        
+    def onConnected(self, address):
+        pass
+        
+    def onInvalidPassword(self, address):
+        pass
+        
+    def onCommand(self, address):
+        pass
