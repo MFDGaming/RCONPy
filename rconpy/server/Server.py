a9e5b0742d9684c11fa37776a37b7fe3926a62a2
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-09-30 20:42:17 +0300

    Update Server.py

--- rconpy/server/Server.py
+++ rconpy/server/Server.py
@@ -2,55 +2,60 @@ from rconpy.protocol.AuthPacket import AuthPacket
 from rconpy.protocol.AuthResponsePacket import AuthResponsePacket
 from rconpy.protocol.ExecCommandPacket import ExecCommandPacket
 from rconpy.protocol.ResponsePacket import ResponsePacket
+from rconpy.server.ServerInterface import ServerInterface
 from rconpy.server.ServerSocket import ServerSocket
 from threading import Thread
 
 class Server(Thread):
     address = None
     password = None
+    interface = None
     socket = None
 
-    def __init__(self, address, password):
+    def __init__(self, address, password, interface = None):
         super().__init__()
         self.address = address
         self.password = password
+        if interface != None:
+            self.interface = interface
+        else:
+            self.interface = ServerInterface()
         self.start()
         
-    def handleAuthPacket(self, data):
-        print("CONNECTING...")
+    def handleAuthPacket(self, data, connection, address):
+        self.interface.onOpenConnection(address)
         decodedPacket = AuthPacket()
         decodedPacket.buffer = data
         decodedPacket.decodePayload()
         if decodedPacket.body != self.password:
             id = -1
-            print("INVALID PASSWORD!")
+            self.interface.onInvalidPassword(address)
         else:
             id = decodedPacket.id
         packet = AuthResponsePacket()
         packet.length = decodedPacket.length
         packet.id = id
         packet.encodePayload()
-        print("CONNECTED!")
+        self.interface.onConnected(address)
         return packet.buffer
 
-    def handleExecCommandPacket(self, data):
+    def handleExecCommandPacket(self, data, connection, address):
         decodedPacket = ExecCommandPacket()
         decodedPacket.buffer = data
         decodedPacket.decodePayload()
         packet = ResponsePacket()
         packet.length = decodedPacket.length
         packet.id = decodedPacket.id
-        packet.body = decodedPacket.body
+        packet.body = self.interface.onCommand(decodedPacket.body)
         packet.encodePayload()
-        print(packet.body)
         return packet.buffer
 
     def handle(self, data, connection, address):
         type = int.from_bytes(data[8:11], "little")
         if type == AuthPacket.type:
-            self.socket.sendBuffer(self.handleAuthPacket(data), connection)
+            self.socket.sendBuffer(self.handleAuthPacket(data, connection, address), connection)
         elif type == ExecCommandPacket.type:
-            self.socket.sendBuffer(self.handleExecCommandPacket(data), connection)
+            self.socket.sendBuffer(self.handleExecCommandPacket(data, connection, address), connection)
         else:
             raise Exception(f"Received unknown packet type: {str(type)}")
         
