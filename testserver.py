af05f59c90d98402f301309bb76eeb26b9d3d94c
MFDGaming <38327951+MFDGaming@users.noreply.github.com>
2020-10-01 09:39:12 +0300

    Create testserver.py

---
+++ testserver.py
@@ -0,0 +1,20 @@
+from rconpy.server.Server import Server
+from rconpy.server.ServerInterface import ServerInterface
+
+class TestServer(ServerInterface):
+    def __init__(self):
+        Server(("0.0.0.0", 25565), "12345", self)
+
+    def onOpenConnection(self, address):
+        print("Awaiting RCON connection from " + address[0] + ":" + str(address[1]))
+
+    def onInvalidPassword(self, address):
+        print("Incorrect password from " + address[0] + ":" + str(address[1]))
+
+    def onConnected(self, address):
+        print("Connection established from " + address[0] + ":" + str(address[1]))
+
+    def onCommand(self, command):
+        return command
+
+TestServer()
